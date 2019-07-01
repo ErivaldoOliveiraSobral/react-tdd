19c3548ff88e9d246b0aed0a229302f40d3bca84
Erivaldo Oliveira Sobral - ESB <erivaldo.sobral@iteris.com.br>
2019-07-01 18:30:39 -0300

    Mensagem de erro

--- src/webparts/demoTdd/components/app/DemoTdd.tsx
+++ src/webparts/demoTdd/components/app/DemoTdd.tsx
@@ -1,20 +1,18 @@
 import * as React from 'react';
 import styles from './DemoTdd.module.scss';
+import './DemoTdd.css';
 import { IDemoTddProps } from './IDemoTddProps';
+import { IDemoTddState } from './IDemoTddState';
 import { escape } from '@microsoft/sp-lodash-subset';
 
 import Display from "../component1/counter";
-
-interface IDemoTddState {
-  counter?: number;
-  error?: boolean;
-}
 
 export default class DemoTdd extends React.Component<IDemoTddProps, IDemoTddState> {
   constructor(props: IDemoTddProps) {
     super(props);
     this.state = {
-      counter: 0
+      counter: 0,
+      error: false
     };
   }
   private changeCounter(delta: number): void {
@@ -29,14 +27,12 @@ export default class DemoTdd extends React.Component<IDemoTddProps, IDemoTddStat
       this.setState({ counter: this.state.counter + delta });
   };
   public render(): React.ReactElement<IDemoTddProps> {
+    const errorClass = this.state.error ? '' : 'hidden';
     return (
       <div className={styles.demoTdd} data-test="component-demo-tdd">
         <Display counter={this.state.counter} />
         <h1 data-test="display-contagem">Contagem: {this.state.counter}</h1>
-        {this.state.error ?
-          <p data-test="error-message" style={{ color: 'red' }}>Valor não pode ser negativo</p> 
-          : 
-          ''}
+        <p data-test="error-message" className={errorClass} style={{ color: 'red' }}>Valor não pode ser negativo</p>
         <button
           data-test="button-incremento"
           onClick={() => this.changeCounter(1)}>
--- src/webparts/demoTdd/components/app/IDemoTddState.ts
+++ src/webparts/demoTdd/components/app/IDemoTddState.ts
@@ -1,3 +1,4 @@
 export interface IDemoTddState {
-    counter: number;
+    counter?: number;
+    error?: boolean;
 }
