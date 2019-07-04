816090605834f6974ddaf3f84b67b1d71c97a952
Erivaldo Oliveira Sobral - ESB <erivaldo.sobral@iteris.com.br>
2019-07-04 12:15:33 -0300

    State do Input

--- src/webparts/jotto/components/input/Input.spec.tsx
+++ src/webparts/jotto/components/input/Input.spec.tsx
@@ -29,31 +29,35 @@ describe("Resderização", () => {
             let state = wrapper.state("success");
             expect(state).toBe(false);
         });
-        it("Mudança de state", () => {
-            wrapper = setup({ success: true });
-            let state = wrapper.state("success");
-            expect(state).toBe(true);
-        });
         it("Renderizado input box", () => {
-            wrapper = setup({ success: false });
             let inputBox = findByTestAttr(wrapper, 'input-box');
             expect(inputBox.length).toBe(1);
         });
         it("Renderizado botão de submit", () => {
-            wrapper = setup({ success: false });
             let submitButton = findByTestAttr(wrapper, 'submit-button');
             expect(submitButton.length).toBe(1);
         });
     });
     describe("Quando a palavra é encontrada", () => {
+        let wrapper: ShallowWrapper;
+        beforeEach(() => {
+            wrapper = setup({ success: true });
+        });
         it("Componente renderizado sem erros", () => {
-
+            let component = findByTestAttr(wrapper, 'component-input');
+            expect(component.length).toBe(1);
         });
-        it("Não renderiza input box", () => {
-
+        it("State inicial", () => {
+            let state = wrapper.state("success");
+            expect(state).toBe(true);
         });
-        it("Não renderiza botão de submit", () => {
-
+        it("Renderizado input box", () => {
+            let inputBox = findByTestAttr(wrapper, 'input-box');
+            expect(inputBox.length).toBe(0);
+        });
+        it("Renderizado botão de submit", () => {
+            let submitButton = findByTestAttr(wrapper, 'submit-button');
+            expect(submitButton.length).toBe(0);
         });
     });
 });
