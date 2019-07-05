c9d0e8590e0ae4068b920749ceabbc3c3634d730
Erivaldo Oliveira Sobral - ESB <erivaldo.sobral@iteris.com.br>
2019-07-05 15:26:17 -0300

    State do Input

--- src/webparts/jotto/components/app/Jotto.tsx
+++ src/webparts/jotto/components/app/Jotto.tsx
@@ -4,9 +4,24 @@ import { IJottoProps } from './IJottoProps';
 import Congrats from "../congrats/Congrats";
 import GuessedWords from "../guessedWords/GuessedWords";
 
-interface IJottoState { };
+interface IJottoState { 
+  secretWord: string;
+};
 
 export default class Jotto extends React.Component<IJottoProps, IJottoState> {
+  constructor(props: IJottoProps) {
+    super(props);
+    this.state = {
+      secretWord: ""
+    };
+  };
+  private getScretWord() {
+    return "train";
+  };
+  componentDidMount() {
+    this.setState({ secretWord: this.getScretWord() });
+  };
+  
   public render(): React.ReactElement<IJottoProps> {
     return (
       <div>
